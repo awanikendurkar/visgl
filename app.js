27cd55cd9c069f2253a3591f6cdea2e5744ffff2
Awani <awanikendurkar@gmail.com>
2022-11-27 16:52:30 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,160 @@
+/* global window */
+import React, { useState, useEffect } from "react";
+import { render } from "react-dom";
+import { StaticMap } from "react-map-gl";
+import { AmbientLight, PointLight, LightingEffect } from "@deck.gl/core";
+import DeckGL from "@deck.gl/react";
+import { PolygonLayer } from "@deck.gl/layers";
+import { TripsLayer } from "@deck.gl/geo-layers";
+import axios from "axios";
+
+// make API call
+const getQuery = () => {
+  axios
+    .get("http://localhost:8080", {
+      params: {
+        id: 12345,
+      },
+    })
+    .then((res) => console.log(res));
+};
+
+// Source data CSV
+const DATA_URL = {
+  BUILDINGS:
+    "https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/trips/buildings.json", // eslint-disable-line
+  TRIPS:
+    "https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/trips/trips-v7.json", // eslint-disable-line
+};
+
+const ambientLight = new AmbientLight({
+  color: [255, 255, 255],
+  intensity: 1.0,
+});
+
+const pointLight = new PointLight({
+  color: [255, 255, 255],
+  intensity: 2.0,
+  position: [-74.05, 40.7, 8000],
+});
+
+const lightingEffect = new LightingEffect({ ambientLight, pointLight });
+
+const material = {
+  ambient: 0.1,
+  diffuse: 0.6,
+  shininess: 32,
+  specularColor: [60, 64, 70],
+};
+
+const DEFAULT_THEME = {
+  buildingColor: [74, 80, 87],
+  trailColor0: [253, 128, 93],
+  trailColor1: [23, 184, 190],
+  material,
+  effects: [lightingEffect],
+};
+
+const INITIAL_VIEW_STATE = {
+  longitude: -74,
+  latitude: 40.72,
+  zoom: 13,
+  pitch: 45,
+  bearing: 0,
+};
+
+const MAP_STYLE =
+  "https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json";
+
+const landCover = [
+  [
+    [-74.0, 40.7],
+    [-74.02, 40.7],
+    [-74.02, 40.72],
+    [-74.0, 40.72],
+  ],
+];
+
+export default function App({
+  buildings = DATA_URL.BUILDINGS,
+  trips = DATA_URL.TRIPS,
+  trailLength = 180,
+  initialViewState = INITIAL_VIEW_STATE,
+  mapStyle = MAP_STYLE,
+  theme = DEFAULT_THEME,
+  loopLength = 1800, // unit corresponds to the timestamp in source data
+  animationSpeed = 1,
+}) {
+  const [time, setTime] = useState(0);
+  const [animation] = useState({});
+
+  const animate = () => {
+    setTime((t) => (t + animationSpeed) % loopLength);
+    animation.id = window.requestAnimationFrame(animate);
+  };
+
+  useEffect(() => {
+    animation.id = window.requestAnimationFrame(animate);
+    return () => window.cancelAnimationFrame(animation.id);
+  }, [animation]);
+
+  const layers = [
+    // This is only needed when using shadow effects
+    new PolygonLayer({
+      id: "ground",
+      data: landCover,
+      getPolygon: (f) => f,
+      stroked: false,
+      getFillColor: [0, 0, 0, 0],
+    }),
+    new TripsLayer({
+      id: "trips",
+      data: trips,
+      getPath: (d) => d.path,
+      getTimestamps: (d) => d.timestamps,
+      getColor: (d) => (d.vendor === 0 ? theme.trailColor0 : theme.trailColor1),
+      opacity: 0.3,
+      widthMinPixels: 2,
+      rounded: true,
+      trailLength,
+      currentTime: time,
+
+      shadowEnabled: false,
+    }),
+    new PolygonLayer({
+      id: "buildings",
+      data: buildings,
+      extruded: true,
+      wireframe: false,
+      opacity: 0.5,
+      getPolygon: (f) => f.polygon,
+      getElevation: (f) => f.height,
+      getFillColor: theme.buildingColor,
+      material: theme.material,
+    }),
+  ];
+
+  return (
+    <div className='row' style={{ display: "flex" }}>
+      <div className='column' style={{ float: "left", flex: 70 }}>
+        <DeckGL
+          layers={layers}
+          effects={theme.effects}
+          initialViewState={initialViewState}
+          controller={true}
+          width='75%'
+        >
+          <StaticMap reuseMaps mapStyle={mapStyle} preventStyleDiffing={true} />
+        </DeckGL>
+      </div>
+
+      <div className='column'>
+        <button onClick={getQuery}>Submit</button>
+      </div>
+    </div>
+  );
+}
+
+export function renderToDOM(container) {
+  render(<App />, container);
+}
